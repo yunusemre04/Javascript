ebc84e6943c6e0552b1aab15284f99f010eac14b
Yunus Emre Acar <emreca5439@gmail.com>
2024-06-09 23:34:45 +0300

    Project Currency

--- Project9_CurrencyApp/script.js
+++ Project9_CurrencyApp/script.js
@@ -1,27 +1,47 @@
-let currencies =[];
-fetch(`https://api.frankfurter.app/latest?currencies`)
-.then(res=>res.json())
-.then(data =>{
-    
-    document.querySelector("#item2").innerHTML=`<h3>  1€ = ${data.rates.TRY.toFixed(3)}₺</h3>`;
-    document.querySelector("#item3").innerHTML=`<h3>  1€ = ${data.rates.USD.toFixed(3)}$</h3>`;
-    document.querySelector("#item4").innerHTML=`<h3>  1€ = ${data.rates.GBP.toFixed(3)}£</h3>`
-    let options;
-    // fix that
-    for (let i=0;i<data.rates.length;i++) {
-        currencies.push({});
-        options+=`<option value="${data.rates[currency]}></option>"`
-    }
-    console.log(currencies);
-    
-}
-);
+const list1 = document.querySelector("#list1");
+const list2 = document.querySelector("#list2");
+const calculateBtn = document.querySelector("#cBtn");
+const amount = document.querySelector("#amount");
+const result=document.querySelector(".number");
 
-fetch(`https://api.frankfurter.app/latest?amount=1&from=USD&to=TRY`)
-.then(res=>res.json())
-.then(data=> {
-    console.log(document.querySelector("#item1"));
-    document.querySelector("#item1").innerHTML=`<h3>1$ = ${Object.values(data.rates)}₺</h3>`
-})
-//https://www.frankfurter.app/docs/
-//fetch(`https://${host}/latest?amount=10&from=GBP&to=USD`)
+let currencies;
+connectServer("USD","TRY","item1");
+connectServer("EUR","TRY","item2");
+connectServer("EUR","USD","item3");
+connectServer("EUR","GBP","item4");
+
+
+fetch(`https://api.frankfurter.app/currencies`)
+  .then(res => res.json())
+  .then(data => {
+   
+    currencies = data;
+    console.log(currencies);    
+    for (let currency in currencies) {
+      const option = `<option value="${currency}">${currency}</option>`;
+      list1.insertAdjacentHTML("beforeend", option);
+      list2.insertAdjacentHTML("beforeend", option);
+    }
+ });
+
+calculateBtn.addEventListener("click", () => {
+  console.log(list1.value, list2.value); 
+
+  fetch(`https://api.frankfurter.app/latest?amount=${amount.value}&from=${list1.value}&to=${list2.value}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(Object.values(data.rates));
+        let a=Object.values(data.rates);
+        result.textContent=`${amount.value} ${list1.value}=${(Object.values(data.rates))} ${Object.keys(data.rates)}`;
+        result.parentElement.classList.remove("d-none");
+    });
+});
+
+
+function connectServer(currency1,currency2,location){
+  fetch(`https://api.frankfurter.app/latest?amount=1&from=${currency1}&to=${currency2}`)
+  .then(res=>res.json())
+  .then(data=>{
+   document.querySelector(`#${location}`).innerHTML=`<h3> 1 ${currency1} = ${Object.values(data.rates)} ${currency2}</h3>`;
+  })
+}
